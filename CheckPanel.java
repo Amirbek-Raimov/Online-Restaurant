fcb1c7ad9207c7565c7220a77582fe8fe92d1b2e
Amirbek-Raimov <53548370+Amirbek-Raimov@users.noreply.github.com>
2021-07-28 06:09:48 -0700

    Final Commit

---
+++ CheckPanel.java
@@ -0,0 +1,87 @@
+import java.sql.*;
+import java.util.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.LineBorder;
+
+class CheckPanel extends JPanel implements ActionListener
+{
+	JButton check,cancel;
+	Connection con;
+	Statement stmnt;
+	
+	public CheckPanel()
+	{
+		try
+		{
+			Connection con=DB.getConnection();
+			stmnt = con.createStatement();
+		}
+		
+		catch(SQLException e)
+		{
+			System.out.println("Unknown Error");
+		}
+		
+		catch(Exception eg)
+		{
+			System.out.println("Unknown Error");
+		}		
+	
+		//setLayout(new FlowLayout(FlowLayout.CENTER,50,50));
+		//setLayout(new GridLayout(9,4));
+		//setLayout(new GridBagLayout());
+		
+		setLayout( new FlowLayout());
+		JLabel background = new JLabel(new ImageIcon("images\\6.jpg"));
+		
+		check = new JButton("Check Order");
+		//cancel = new JButton("Cancel Order");
+		add(check);
+		check.setBounds(40,40,700,20);
+        //check.setIcon(new ImageIcon("images\\home.png"));
+		
+		//add(cancel);
+		add(background);
+		check.addActionListener(this);
+		//cancel.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent a)
+	{
+		if(a.getSource() == check)
+		{
+			ConfirmFood o = new ConfirmFood();
+		}
+		else
+		{
+			try
+			{
+				
+				//Window window = SwingUtilities.getWindowAncestor(this);
+				//window.dispose();
+				//HomeDelivery s = new HomeDelivery();
+				int ans = JOptionPane.showConfirmDialog(null," Are you sure ?",null, JOptionPane.YES_NO_OPTION);
+				
+				if (ans == JOptionPane.YES_OPTION)
+				{
+					stmnt.executeUpdate("TRUNCATE `temp_order`");
+					JOptionPane.showMessageDialog(null," Order cancelled !!! ");
+				}
+			}
+			
+			catch(SQLException e)
+			{
+			    System.out.println("Unknown Error");
+			}
+			
+			catch(Exception eg)
+			{
+				System.out.println("Unknown Error");
+			}
+		}
+	}
+}
---
+++ Delivery.java
@@ -0,0 +1,58 @@
+import java.sql.*;
+import java.util.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.LineBorder;
+
+class Delivery extends JFrame
+{
+	JTabbedPane menu;
+	JPanel pizza,pastry,drinks,confirm;
+	Connection con;
+	Statement stmnt;
+	
+	public Delivery()
+	{
+		try
+		{
+			Connection con=DB.getConnection();
+			stmnt = con.createStatement();
+			stmnt.executeUpdate("TRUNCATE `temp_order`");
+		}
+		
+		catch(SQLException e)
+		{
+			System.out.println("Unknown Error");
+		}
+		
+		catch(Exception eg)
+		{
+			System.out.println("Unknown Error");
+		}		
+		
+		setSize(800,800);
+		setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		menu = new JTabbedPane();
+	
+		menu.addTab("Fast Food",new JScrollPane(new FastFoodPage(),JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
+		
+		menu.addTab("Pizza",new JScrollPane(new PizzaPage(),JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
+		
+		menu.addTab("Dessert",new JScrollPane(new DessertPage(),JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
+		
+		menu.addTab("Beverage",new JScrollPane(new BeveragePage(),JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
+		
+		menu.addTab("Confirm your Order",new CheckPanel());
+		
+		
+		
+		add(menu);
+	}
+	
+	
+}
